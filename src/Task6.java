3cdbb50c9c35d54c6c8b15b724a9358481e5b18e
PanchenkoDenis <toroko0071234@gmail.com>
2024-04-18 20:46:30 +0400

    add task 6

---
+++ src/Task6.java
@@ -0,0 +1,12 @@
+public class Task6 {
+    public static void main(String[] args) {
+        final int W = 5;
+        final int H = 10;
+        System.out.println("Task6");
+        int s = W * H;
+        int p = (W + H) * 2;
+        System.out.println("square - " + s);
+        System.out.println("Perimeter - " + p);
+
+}
+}
