f6c7c4c2bf7a768baa533f145b35b5d16c3e214a
PanchenkoDenis <toroko0071234@gmail.com>
2024-04-18 19:54:55 +0400

    add task 3

---
+++ src/Task3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Task3");
+        Scanner sc = new Scanner(System.in);
+        float n = Math.round(sc.nextFloat());
+        System.out.println("Result=" + n);
+    }
+}
