f7e4dcd6251dceac0b3a81a9c11ffa8b7a1e8e3e
PanchenkoDenis <toroko0071234@gmail.com>
2024-04-18 20:37:20 +0400

    add task 3

---
+++ src/Task5.java
@@ -0,0 +1,13 @@
+public class Task5 {
+
+    public static final int RADIUS = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Task5");
+        double area = Math.PI * (RADIUS * RADIUS);
+        System.out.println(" Площадь круга равна: " + Math.round(area));
+        double circumference = Math.PI * 2 RADIUS;
+        System.out.println("Длина окружности равна: " + Math.round(circumference));
+        }
+    }
+}
